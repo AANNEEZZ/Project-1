d9d3d04e6100af14bd7259a34aa17ed1481af237
Zaelgrad <aniztamu@gmail.com>
2024-04-23 11:35:34 +0545

    Update - 6

--- invoice_details.py
+++ invoice_details.py
@@ -1,12 +1,24 @@
 import datetime
-import pyfiglet
-from pyfiglet import Figlet
 
 def invoice_written(name, duration, kitta_number, city, direction, area, price):
     date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open('Invoice.txt','w') as file:
         file.write("FROM:\n")
-        file.write(pyfiglet.figlet_format("Techno Property", justify="center", font="avatar", width=110))
+        file.write(r'''
+        
+ /$$$$$$$$                  /$$                                 /$$$$$$$                                                     /$$              
+|__  $$__/                 | $$                                | $$__  $$                                                   | $$              
+   | $$  /$$$$$$   /$$$$$$$| $$$$$$$  /$$$$$$$   /$$$$$$       | $$  \ $$ /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$   /$$   /$$
+   | $$ /$$__  $$ /$$_____/| $$__  $$| $$__  $$ /$$__  $$      | $$$$$$$//$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$|_  $$_/  | $$  | $$
+   | $$| $$$$$$$$| $$      | $$  \ $$| $$  \ $$| $$  \ $$      | $$____/| $$  \__/| $$  \ $$| $$  \ $$| $$$$$$$$| $$  \__/  | $$    | $$  | $$
+   | $$| $$_____/| $$      | $$  | $$| $$  | $$| $$  | $$      | $$     | $$      | $$  | $$| $$  | $$| $$_____/| $$        | $$ /$$| $$  | $$
+   | $$|  $$$$$$$|  $$$$$$$| $$  | $$| $$  | $$|  $$$$$$/      | $$     | $$      |  $$$$$$/| $$$$$$$/|  $$$$$$$| $$        |  $$$$/|  $$$$$$$
+   |__/ \_______/ \_______/|__/  |__/|__/  |__/ \______/       |__/     |__/       \______/ | $$____/  \_______/|__/         \___/   \____  $$
+                                                                                            | $$                                     /$$  | $$
+                                                                                            | $$                                    |  $$$$$$/
+                                                                                            |__/                                     \______/ 
+
+        ''')
         file.write("TO:\n")
         file.write(f"{name}\n")
         file.write(f"Kitta Number: {kitta_number}\n")
@@ -16,11 +28,6 @@ def invoice_written(name, duration, kitta_number, city, direction, area, price):
         file.write(f"Price(in NPR): {price}\n")
         file.write(f"Duration: {duration} months\n")
         file.write(f"Date: {date}\n")
-
-def print_invoice_data():
-    with open('Invoice.txt', 'r') as file:
-        invoice_data = file.read()
-        print(invoice_data)
 
 def edit_invoice_data(duration, new_kitta_number, new_city, new_direction, new_area, new_price):
     date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -33,6 +40,25 @@ def edit_invoice_data(duration, new_kitta_number, new_city, new_direction, new_a
         file.write(f"Price(in NPR): {new_price}\n")
         file.write(f'Duration: {duration} months\n')
         file.write(f"Date: {date}\n")
+
+def calculate_total():
+    total_amount = 0
+    with open(r'Invoice.txt', 'r') as file:
+        for line in file:
+            if "Price(in NPR):" in line:
+                price = float(line.split(":")[1].strip())
+                total_amount += price
+    
+    # Append total amount to the invoice file
+    with open('Invoice.txt', 'a') as file:
+        file.write(f"\n\n{' '*18}Total Amount = {total_amount}\n")
+
+
+
+def print_invoice_data():
+    with open('Invoice.txt', 'r') as file:
+        invoice_data = file.read()
+        print(invoice_data)        
 
 def return_invoice(name, kitta_number, city_district, direction, area, price):
     with open(r'return invoice.txt', 'w') as file:
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import pyfiglet
-import tabulate
-from tabulate import tabulate
+import datetime
 
-from pyfiglet import Figlet
-
-from land_info import land_info, edit_land_info
-from invoice_details import invoice_written, print_invoice_data, edit_invoice_data, return_invoice
+from land_info import land_info, edit_land_info, display_banner, record
+from invoice_details import invoice_written, print_invoice_data, edit_invoice_data, return_invoice, calculate_total
 from management import return_land, record_customer_details, check_customer_details,count_customer_details, read_customer_details
 
 
-print(pyfiglet.figlet_format("Techno Property", justify="center", font="starwars", width=110))
+display_banner()
 print("\n\t\t\t\t\tHello Customer!")
 
 while True:
@@ -26,20 +22,31 @@ while True:
     question_1 = int(input("\nEnter the above given options:  "))
 
     if question_1 == 1:
-        print(tabulate(lands, headers=headers, tablefmt='pretty'))
-        
+        # Print all lands
+        print("{:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format(*headers))
+        print("-" * 90)
+        for land in lands:
+            print("{:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format(*land))
+            
     elif question_1 == 2:
         Available_lands = [land for land in lands if 'Available' in land]
-        print(tabulate(Available_lands, headers=headers, tablefmt='pretty'))
+        print("{:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format(*headers))
+        print("-" * 90)
+        for land in Available_lands:
+            print("{:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format(*land))    
 
     elif question_1 == 3:
         not_available_lands = [land for land in lands if 'Not_Available' in land]
-        print(tabulate(not_available_lands, headers=headers, tablefmt='pretty'))
+        print("{:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format(*headers))
+        print("-" * 90)
+        for land in not_available_lands:
+            print("{:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format(*land))
 
     elif question_1 == 4:
         Available_lands = [land for land in lands if 'Available' in land]
         rent_question_1 = input("\nWhich land would you want to rent? (Enter the Kitta number): ")
         rented_land = [land for land in Available_lands if land[0] == int(rent_question_1)]
+        date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         
         if rented_land:
             name = input("\nWhat is your name?: ")
@@ -49,10 +56,10 @@ while True:
             print("\nRent successful")
             invoice_written(name, duration, rented_land[0], rented_land[1], rented_land[2], rented_land[3], rented_land[4])
             edit_land_info(r'Land.txt', rented_land[0])
-
+            record(r'record.txt', name, rent_question_1, date, duration)
             Available_lands = [land for land in lands if 'Available' in land]
-            
             while True:
+                Available_lands = [land for land in lands if 'Available' in land]
                 rent_question_2 = input("\nWould you like to rent another land? (y/n): ")
                 if rent_question_2.lower() == 'y':
                     rent_question_3 = input("\nWhich land do you want to rent? (Enter the Kitta number): ")
@@ -64,11 +71,13 @@ while True:
                         edit_invoice_data(duration_2, rented_land_2[0], rented_land_2[1], rented_land_2[2], rented_land_2[3], rented_land_2[4])
                         edit_land_info(r'Land.txt', rented_land_2[0])
                         record_customer_details(r'Customers.txt', name, rent_question_3)
+                        record(r'record.txt', name, rent_question_3, date, duration_2)
                     else:
                         print("That land is not available")
                 else:
                     break
-
+        
+            calculate_total()
             print_invoice_data()
         else:
             print("That land is not available")
@@ -76,30 +85,31 @@ while True:
 
     elif question_1 == 5:
         return_question = input("\nWhat is your name?: ")
-        while True:
-            customer_details = read_customer_details(r'Customers.txt', return_question)
-            match = [customer for customer in customer_details if customer[0] == return_question]
-            counter = count_customer_details(r'Customers.txt', return_question)
-            print(f"\n{return_question} has rented {counter} under their name.")
-            for customer in customer_details:
-                print(f"{customer[1]}")
-            return_question_1 = int(input("\nEnter the Kitta number of the land you want to return: "))
-            if check_customer_details(r'Customers.txt', return_question, return_question_1):
-                return_land(r'Land.txt', return_question_1)
-                print(f"Kitta number {return_question_1} has been returned by {return_question}.")
-                # Generate return invoice
-                returned_land = [land for land in lands if land[0] == return_question_1][0]
-                return_invoice(return_question, returned_land[0], returned_land[1], returned_land[2], returned_land[3], returned_land[4])
-            else:
-                print(f"No rent for Kitta number {return_question_1} has been made by {return_question}.")
-            
-            if count_customer_details(r'Customers.txt', return_question) > 0:
-                return_question_2 = input("Would you like to return other lands? (y/n): ")
-                if return_question_2.lower() != "y":
-                    break
+        customer_details = read_customer_details(r'Customers.txt', return_question)
+        match = [customer for customer in customer_details if customer[0] == return_question]
+        counter = count_customer_details(r'Customers.txt', return_question)
+        if counter > 0: 
+            while True:
+                print(f"\n{return_question} has rented {counter} under their name.")
+                for customer in customer_details:
+                    print(f"{customer[1]}")
+                return_question_1 = int(input("\nEnter the Kitta number of the land you want to return: "))
+                if check_customer_details(r'Customers.txt', return_question, return_question_1):
+                    return_land(r'Land.txt', return_question_1)
+                    print(f"Kitta number {return_question_1} has been returned by {return_question}.")
+                    # Generate return invoice
+                    returned_land = [land for land in lands if land[0] == return_question_1][0]
+                    return_invoice(return_question, returned_land[0], returned_land[1], returned_land[2], returned_land[3], returned_land[4])
+                else:
+                    print(f"No rent for Kitta number {return_question_1} has been made by {return_question}.")
+                
+                if count_customer_details(r'Customers.txt', return_question) > 0:
+                    return_question_2 = input("Would you like to return other lands? (y/n): ")
+                    if return_question_2.lower() != "y":
+                        break
 
-            else:
-                break    
+                else:
+                    break    
             
 
 
